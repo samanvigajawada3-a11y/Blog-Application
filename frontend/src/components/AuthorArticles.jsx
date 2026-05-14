649d59c1beddbf0e64682932abb946e5adbf762c
samanvigajawada3-a11y <samanvigajawada3@gmail.com>
2026-05-14 21:19:14 +0530

    Update AuthorArticles.jsx

--- frontend/src/components/AuthorArticles.jsx
+++ frontend/src/components/AuthorArticles.jsx
@@ -29,7 +29,7 @@ function AuthorArticles() {
       try{
         setLoading(true)
         // read articles of current author
-        let res = await axios.get("http://localhost:4000/author-api/articles",{withCredentials:true})
+        let res = await axios.get("https://blog-application-11f0.onrender.com/author-api/articles",{withCredentials:true})
         if(res.status === 200){
           setArticles(res.data.payload)
         }
